f3cbc94c5afdedfc35c9dad6932c500297903563
Akshay <akshaykomade012345@gmail.com>
2023-08-21 23:10:58 +0530

    Added Dark Mode

--- script.js
+++ script.js
@@ -23,4 +23,13 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   });
+  document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const body = document.body;
+  
+    darkModeToggle.addEventListener("click", function() {
+      body.classList.toggle("dark-mode");
+      body.classList.toggle("light-mode");
+    });
+  });
   
